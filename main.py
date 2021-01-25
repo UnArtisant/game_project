c7dde390108bcd23e0898cc6099e343fe36a0090
Unaxe <74629449+Unaxe@users.noreply.github.com>
2021-01-25 05:10:19 +0100

    Creation de la premiere fenêtre et premieres ressources de base à changer

--- main.py
+++ main.py
@@ -6,4 +6,29 @@ Marco ....
 Ahyl ....
 Zyad .....
 """
-print("hello world")
+import pygame
+pygame.init()
+
+
+
+#generer la fenetre du jeu
+pygame.display.set_caption("DripFighters")
+screen = pygame.display.set_mode((1080,720))
+background = pygame.image.load('src/ressources_graphiques/bg.jpg')
+
+
+running = True
+
+while running:
+
+    #Application du fond d'écran
+    screen.blit(background,(0,0))
+
+    #mise a jour de l'écran
+    pygame.display.flip()
+
+    #Fermeture du programme quand on clique su la croix
+    for  event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
