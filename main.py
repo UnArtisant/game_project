b77f335f7ba3fbf71408c24fbac333a859480c40
Unaxe <74629449+Unaxe@users.noreply.github.com>
2021-01-25 05:35:19 +0100

    Debut de creation de la class Joueur 1

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ Zyad .....
 import pygame
 pygame.init()
 
+class Player1(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.fireBallDmg = 5
+        self.fistDmg = 10
+        self.paradeReduction = 0.8
+        self.velocity = 5
+        self.image = pygame.image.load("src/ressources_graphiques/player.png")
+        self.rect = self.image.get_rect()
+        self.rect.x = 500
+        self.rect.y = 360
 
 
 #generer la fenetre du jeu
@@ -16,13 +28,15 @@ pygame.display.set_caption("DripFighters")
 screen = pygame.display.set_mode((1080,720))
 background = pygame.image.load('src/ressources_graphiques/bg.jpg')
 
+player1 = Player1()
 
 running = True
-
 while running:
 
     #Application du fond d'écran
     screen.blit(background,(0,0))
+
+    screen.blit(player1.image, player1.rect)
 
     #mise a jour de l'écran
     pygame.display.flip()
